1e9dee69ce727c9abd2b3867b184d94c2176b3d3
Enmanuel Martins <eel2000martins@hotmail.com>
2022-01-11 00:48:34 +0000

    html css javascript finalizado front end por ahora listo

--- src/js/app.js
+++ src/js/app.js
@@ -11,3 +11,53 @@ document.addEventListener('DOMContentLoaded', function(){
         .bindPopup('Aqui estamos Extreme Gym Fit.')
         .openPopup();
 })
+
+
+document.addEventListener('DOMContentLoaded', function(){
+    eventListener();
+
+    darkMode();
+});
+
+function darkMode(){
+
+    const prefiereDarkMode = window.matchMedia('(prefers-color-scheme: dark)');
+
+    if(prefiereDarkMode.matches){
+        document.body.classList.add('dark-mode');
+    } else{
+        document.body.classList.remove('dark-mode');
+    }
+
+    prefiereDarkMode.addEventListener('change', function(){
+        if(prefiereDarkMode.matches){
+            document.body.classList.add('dark-mode');
+        } else{
+            document.body.classList.remove('dark-mode');
+        }
+    });
+
+    const botonDarkMode = document.querySelector('.dark-mode-boton');
+
+    botonDarkMode.addEventListener('click', function(){
+        document.body.classList.toggle('dark-mode');
+    });
+}
+
+function eventListener(){
+    const mobileMenu = document.querySelector('.mobile-menu');
+
+    mobileMenu.addEventListener('click', navegacionResponsive)
+}
+
+function navegacionResponsive(){
+    const navegacion = document.querySelector('.navegacion');
+
+    navegacion.classList.toggle('mostrar');
+
+    // if(navegacion.classList.contains('mostrar')){
+    //     navegacion.classList.remove('mostrar');
+    // } else {
+    //     navegacion.classList.add('mostrar');
+    // }
+}
