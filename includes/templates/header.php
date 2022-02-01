63aef5f94f50039b6f8e5a628926ef5f6a818722
Enmanuel Martins <eel2000martins@hotmail.com>
2022-02-01 22:36:43 +0000

    colocarl el favicon para la navegacion para el logo en internet y eliminar el usuario y crear el usuaio de josue

--- includes/templates/header.php
+++ includes/templates/header.php
@@ -16,6 +16,7 @@
     <link rel="stylesheet" href="/build/css/app.css">
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A==" crossorigin="" />
+    <link rel="icon" href="/build/img/favicon.ico">
 </head>
 <body>
 
